92457cb9fad64f29aedb2b1d78d65eb46f9a421d
DainPixel <166203518+DainPixel@users.noreply.github.com>
2025-09-22 20:21:37 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,39 @@
-// 중앙 로고 ↔ 메인 메뉴 hover 연동
-function linkHover(letterSelector, menuSelector, color) {
+function linkHover(letterSelector, floatMenuSelector, headerSelector, color) {
   const letter = document.querySelector(letterSelector);
-  const menu = document.querySelector(menuSelector);
+  const floatMenu = document.querySelector(floatMenuSelector);
+  const header = document.querySelector(headerSelector);
 
   function activate() {
-    letter.style.color = color;
-    menu.style.color = color;
-    menu.style.transform = "scale(1.2)";
-  }
-  function deactivate() {
-    letter.style.color = "";
-    menu.style.color = "";
-    menu.style.transform = "";
+    if (letter) letter.style.color = color;
+    if (floatMenu) {
+      floatMenu.style.color = color;
+      floatMenu.style.transform = "scale(1.2)";
+    }
+    if (header) header.style.color = color;
   }
 
-  if (letter && menu) {
-    letter.addEventListener("mouseenter", activate);
-    letter.addEventListener("mouseleave", deactivate);
-    menu.addEventListener("mouseenter", activate);
-    menu.addEventListener("mouseleave", deactivate);
+  function deactivate() {
+    if (letter) letter.style.color = "";
+    if (floatMenu) {
+      floatMenu.style.color = "";
+      floatMenu.style.transform = "";
+    }
+    if (header) header.style.color = "";
   }
+
+  [letter, floatMenu, header].forEach(el => {
+    if (el) {
+      el.addEventListener("mouseenter", activate);
+      el.addEventListener("mouseleave", deactivate);
+    }
+  });
 }
 
-// 연결 설정 (중앙 로고 ↔ 메인 메뉴만)
-linkHover(".letter-a", ".float-menu.about", "red");
-linkHover(".letter-p", ".float-menu.project", "orange");
-linkHover(".letter-x", ".float-menu.experience", "royalblue");
-linkHover(".letter-e", ".float-menu.education", "green");
+// About ↔ A
+linkHover(".letter-a", ".float-menu.about", ".full-menu .about", "red");
+// Project ↔ P
+linkHover(".letter-p", ".float-menu.project", ".full-menu .project", "orange");
+// Experience ↔ X
+linkHover(".letter-x", ".float-menu.experience", ".full-menu .experience", "royalblue");
+// Education ↔ E
+linkHover(".letter-e", ".float-menu.education", ".full-menu .education", "green");
